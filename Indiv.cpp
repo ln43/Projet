c1b3bcf81a7d4dd7fc4d88b928529720621228c4
Hélène VASSAL <helene.vassal@insa-lyon.fr>
2016-03-08 16:36:45 +0100

    Ajout System Indiv main et param

---
+++ Indiv.cpp
@@ -0,0 +1,94 @@
+
+// ===========================================================================
+//                                  Includes
+// ===========================================================================
+#include "Indiv.h"
+
+using namespace std;
+
+float Indiv::pMut_=0;
+float Indiv::pDeath_=0;
+float Indiv::wMin_=0;
+float Indiv::RAA_=0;
+float Indiv::RAB_=0;
+float Indiv::RBB_=0;
+float Indiv::RBC_=0;
+
+
+// ===========================================================================
+//                                Constructors
+// ===========================================================================
+  Indiv::Indiv(){
+    genot_=0;
+    A_=0;
+    B_=0;
+    C_=0;
+    fit_=0;
+    vivant=false;
+  }
+
+  Indiv::Indiv(int genot){
+    genot_=genot;
+    A_=0;
+    B_=1;
+    C_=2;
+    if (genot_==0){
+      fit_=B_;
+    }else{
+      fit_=C_;
+    }
+    //~ fit_=0;
+    vivant=true;
+  }
+// ===========================================================================
+//                                 Destructor
+// ===========================================================================
+  Indiv::~Indiv(){}
+
+// ===========================================================================
+//                               Public Methods
+// ===========================================================================
+  void Indiv::death(){
+    int p=rand()%101;
+    if(p<pDeath_*100){vivant=false;}
+  }
+
+  void Indiv::mute(){
+    int p=rand()%101;
+    if(p<pMut_*100){
+      if(genot_==0){genot_==1;}
+      else{genot_==0;}
+    }
+  }
+
+  void Indiv::set_fitness(){
+    if (genot_==0){
+      fit_=B_;
+    }else{
+      fit_=C_;
+    }
+    if(fit_<wMin_){
+      fit_=0;
+    }
+  }
+
+  float Indiv::metabolA(float aout){
+    float Aout=aout;
+    for (float t=0;t<1;t+=0.1){
+      Aout=Aout-Aout*RAA_*0.1;
+      A_=A_+(Aout*RAA_-A_*RAB_)*0.1;
+      B_=B_+A_*RAB_*0.1;
+    }
+    return Aout;
+  }
+
+  float Indiv::metabolB(float bout){
+    float Bout=bout;
+    for (float t=0;t<1;t+=0.1){
+      Bout=Bout-Bout*RBB_*0.1;
+      B_=B_+(Bout*RBB_-B_*RBC_)*0.1;
+      C_=C_+B_*RBC_*0.1;
+    }
+    return Bout;
+  }
+
---
+++ main.cpp
@@ -0,0 +1,62 @@
+// ===========================================================================
+//                                  Includes
+// ===========================================================================
+
+#include "System.h"
+#include "Envir.h"
+#include "Indiv.h"
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+using namespace std;
+
+
+// ===========================================================================
+//                            Function declarations
+// ===========================================================================
+
+
+// ===========================================================================
+//                                    MAIN
+// ===========================================================================
+int main() {
+  srand(time(NULL));
+
+  cout<<"START"<<endl;
+
+  ifstream f;
+  f.open("params.txt",ios::in);
+
+  float p_mut,p_death,RAA,RBB,RAB,RBC,w_min,d;
+  int w,h;
+  string in1,in2,in3,in4,in5,in6,in7,in8,in9,in10;
+  f>>in1>>p_mut;
+  f>>in2>>p_death;
+  f>>in3>>w_min;
+  f>>in4>>RAA;
+  f>>in5>>RAB;
+  f>>in6>>RBB;
+  f>>in7>>RBC;
+  f>>in8>>d;
+  f>>in9>>w;
+  f>>in10>>h;
+  f.close();
+
+  Indiv::set_params(p_mut,p_death,w_min,RAA,RAB,RBB,RBC);
+  Envir::set_params(d,w,h);
+  System::set_params(w,h);
+
+  System S;
+  System S1(4,1);
+  S1.affichageFit();
+//  cout<<"_ _ _ _ _ _ _ _ _ _ _ "<<endl;
+//  S1.begin(10);
+//  cout<<"_ _ _ _ _ _ _ _ _ _ _ "<<endl;
+//  S1.affichageViv();
+//  cout<<"_ _ _ _ _ _ _ _ _ _ _ "<<endl;
+//  S1.affichageFit();
+  return EXIT_SUCCESS;
+}
+
