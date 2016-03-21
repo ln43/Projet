e75bf975629d762eff509745d816ea9a66b429a9
VASSAL Hélène <helene.vassal@insa-lyon.fr>
2016-03-21 18:02:29 +0100

    R

--- graphe.R
+++ graphe.R
@@ -2,9 +2,21 @@ rm(list=ls())
 
 d=read.table("data.txt",col.names=c("T","Ainit","Etat"),header=T)
 
+#View3
+etat <- factor(d$"Etat")
+mescouleurs <- rainbow(length(levels(etat)))
+plot(d$"T", d$"Ainit", pch = 20, col = mescouleurs[etat])
+legend("topleft", inset = 0.02, pch = 19, legend = levels(etat), col = mescouleurs)
 
+#View 1
+library(car) 
+scatterplot(Ainit ~ T | Etat, data=d,
+            xlab="T", ylab="Ainit",
+            main="Simulation") 
+
+#View 2
+#install.packages("ggplot2")
+library(ggplot2)
 p=ggplot(data=d, aes(x=T, y=Ainit, colour=Etat))
 p= p + geom_raster(aes(fill = Etat),hjust=0.5,vjust=0.5, interpolate=FALSE)
 print(p)
-
-#install.packages("ggplot2")
